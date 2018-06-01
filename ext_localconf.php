3c4d4517a824266e8d2645f0e39fced13eac3ac0
Franz Holzinger <holzinger@ttproducts.de>
2018-06-01 22:21:02 +0200

    * bugfix in composer.json dependency from static_info_tables

--- ext_localconf.php
+++ ext_localconf.php
@@ -21,26 +21,25 @@ if (!defined ('PATH_BE_STATICINFOTABLESTAXES_REL')) {
     define('PATH_BE_STATICINFOTABLESTAXES_REL', call_user_func($emClass . '::extRelPath', $_EXTKEY));
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['tables']['static_taxes'] =
-    array (
-        'label_fields' => array (
-            'tx_name_##', 'tx_name_en'
-        ),
-        'isocode_field' => array (
-            'tx_code', 'tx_country_iso_##', 'tx_zn_code'
-        )
-    );
-
 // constants for the TCA fields
 
 if (version_compare(TYPO3_version, '8.0.0', '>=')) {
-    // 'label' => 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:LGL.starttime',
+    // for a TCA 'label' => 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:LGL.starttime',
 
     define('STATICINFOTABLESTAXES_LANGUAGE_LGL', 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:LGL.');
 } else {
-    // 'label' => 'LLL:EXT:lang/locallang_general.php:LGL.starttime',
+    // for a TCA 'label' => 'LLL:EXT:lang/locallang_general.php:LGL.starttime',
     define('STATICINFOTABLESTAXES_LANGUAGE_LGL', 'LLL:EXT:lang/locallang_general.php:LGL.');
 }
 
 
-    
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['tables']['static_taxes'] =
+    array (
+        'label_fields' => array(
+            'tx_name_##', 'tx_name_en'
+        ),
+        'isocode_field' => array(
+            'tx_code', 'tx_country_iso_##', 'tx_zn_code'
+        )
+    );
+
