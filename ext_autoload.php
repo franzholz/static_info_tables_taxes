c07772d2b61542e5658cd6b12a807818cb369424
Franz Holzinger <holzinger@ttproducts.de>
2018-08-07 14:59:55 +0200

    * remove the file ext_autoload.php

--- ext_autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$extensionPath = t3lib_extMgm::extPath('static_info_tables_taxes');
-return array(
-	'JambageCom\\StaticInfoTablesTaxes\\Api\\TaxApi' => $extensionPath . 'Classes/Api/TaxApi.php',
-	'JambageCom\\StaticInfoTablesTaxes\\Api\\StaticInfoApi' => $extensionPath . 'Classes/Api/StaticInfoApi.php',
-);
-
