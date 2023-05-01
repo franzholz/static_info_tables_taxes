d5cff441ceae7eafbe6d3199dd47070180ab9e7b
Franz Holzinger <franz.holzinger@festwein.de>
2023-05-01 23:42:49 +0200

    remove standard fields which are in TCA

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,12 +2,6 @@
 # Table structure for table "static_taxes"
 #
 CREATE TABLE static_taxes (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-	hidden tinyint(4) DEFAULT '0' NOT NULL,
-	starttime int(11) DEFAULT '0' NOT NULL,
-	endtime int(11) DEFAULT '0' NOT NULL,
 	tx_country_iso_2 char(2) DEFAULT '' NOT NULL,
 	tx_country_iso_3 char(3) DEFAULT '' NOT NULL,
 	tx_zn_code varchar(45) DEFAULT '',
@@ -17,7 +11,6 @@ CREATE TABLE static_taxes (
 	tx_rate_uid int(11) DEFAULT '0' NOT NULL,
 	tx_priority tinyint(3) unsigned DEFAULT '0',
 
-	PRIMARY KEY (uid),
 	KEY cn_iso_3 (tx_country_iso_3),
 	KEY tx_rate_uid (tx_rate_uid)
 );
@@ -27,17 +20,10 @@ CREATE TABLE static_taxes (
 # Table structure for table "static_tax_rates"
 #
 CREATE TABLE static_tax_rates (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-	hidden tinyint(4) DEFAULT '0' NOT NULL,
-	starttime int(11) DEFAULT '0' NOT NULL,
-	endtime int(11) DEFAULT '0' NOT NULL,
 	title varchar(255) DEFAULT '' NOT NULL,
 	tx_rate decimal(19,2) DEFAULT '0.00' NOT NULL,
 	category tinyint(3) unsigned DEFAULT '1',
 
-	PRIMARY KEY (uid),
 	KEY title (title)
 );
 
@@ -46,15 +32,11 @@ CREATE TABLE static_tax_rates (
 # Table structure for table 'static_taxes_mm_rates'
 #
 CREATE TABLE static_taxes_mm_rates (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
 	uid_local int(11) DEFAULT '0' NOT NULL,
 	uid_foreign int(11) DEFAULT '0' NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
 	sorting int(10) DEFAULT '0' NOT NULL,
 	foreignsort int(10) DEFAULT '0' NOT NULL,
 
-	PRIMARY KEY (uid),
 	KEY uid_local (uid_local),
 	KEY uid_foreign (uid_foreign),
 	KEY sorting (sorting),
@@ -66,16 +48,9 @@ CREATE TABLE static_taxes_mm_rates (
 # Table structure for table "static_tax_categories"
 #
 CREATE TABLE static_tax_categories (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-	hidden tinyint(4) DEFAULT '0' NOT NULL,
-	starttime int(11) DEFAULT '0' NOT NULL,
-	endtime int(11) DEFAULT '0' NOT NULL,
 	title varchar(255) DEFAULT '' NOT NULL,
 	parentid int(11) DEFAULT '0' NOT NULL,
 
-	PRIMARY KEY (uid),
 	KEY parent (parentid),
 	KEY title (title)
 );
@@ -85,18 +60,11 @@ CREATE TABLE static_tax_categories (
 # Table structure for table 'static_tax_rates_mm_categories'
 #
 CREATE TABLE static_tax_rates_mm_categories (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-	hidden tinyint(4) DEFAULT '0' NOT NULL,
-	starttime int(11) DEFAULT '0' NOT NULL,
-	endtime int(11) DEFAULT '0' NOT NULL,
 	uid_local int(11) DEFAULT '0' NOT NULL,
 	uid_foreign int(11) DEFAULT '0' NOT NULL,
 	sorting int(10) DEFAULT '0' NOT NULL,
 	foreignsort int(10) DEFAULT '0' NOT NULL,
 
-	PRIMARY KEY (uid),
 	KEY uid_local (uid_local),
 	KEY uid_foreign (uid_foreign),
 	KEY sorting (sorting),
